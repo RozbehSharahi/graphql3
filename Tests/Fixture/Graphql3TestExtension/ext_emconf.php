ca25f7b0b1337a6fb2f4fbbc9ce6ac1612ec43c9
Rozbeh Chiryai Sharahi <rozbeh.sharahi@gmail.com>
2022-07-14 18:32:26 +0100

    [TASK] Add test extension to build and tests

---
+++ Tests/Fixture/Graphql3TestExtension/ext_emconf.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @noinspection PhpUndefinedVariableInspection */
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'GraphQL Test Extension 3',
+    'description' => 'Graphql3 does not provide much out of the box. This extension was made for testing.',
+    'author' => 'Rozbeh Chiryai Sharahi',
+    'author_email' => 'rozbeh.sharahi+graphql3@gmail.com',
+    'state' => 'alpha',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => ['graphql3' => '0-99'],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
