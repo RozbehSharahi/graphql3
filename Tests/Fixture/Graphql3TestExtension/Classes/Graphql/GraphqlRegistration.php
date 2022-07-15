fd83f5ff30f3fe72d5087a996e001557bfa36efb
Rozbeh Chiryai Sharahi <rozbeh.sharahi@gmail.com>
2022-07-15 22:57:06 +0100

    [TASK] Extend graphql test extension
    Using RegisterBasedQueryType now.

--- Tests/Fixture/Graphql3TestExtension/Classes/Graphql/GraphqlRegistration.php
+++ Tests/Fixture/Graphql3TestExtension/Classes/Graphql/GraphqlRegistration.php
@@ -2,18 +2,30 @@
 
 namespace RozbehSharahi\Graphql3TestExtension\Graphql;
 
-use RozbehSharahi\Graphql3\Builder\NoopSchemaBuilder;
+use GraphQL\Type\Schema;
+use RozbehSharahi\Graphql3\Domain\Model\GraphqlNode;
+use RozbehSharahi\Graphql3\Registry\QueryFieldRegistry;
 use RozbehSharahi\Graphql3\Registry\SchemaRegistry;
+use RozbehSharahi\Graphql3\Type\RegistryBasedQueryType;
 use TYPO3\CMS\Core\SingletonInterface;
 
 class GraphqlRegistration implements SingletonInterface
 {
-    public function __construct(protected SchemaRegistry $registry, protected NoopSchemaBuilder $noopSchemaBuilder)
-    {
+    public function __construct(
+        protected SchemaRegistry $registry,
+        protected QueryFieldRegistry $queryFieldRegistry,
+        protected RegistryBasedQueryType $registryBasedQueryType
+    ) {
     }
 
     public function register(): void
     {
-        $this->registry->register($this->noopSchemaBuilder->build());
+        $this->queryFieldRegistry
+            ->register(GraphqlNode::create('noop')->withResolver(fn () => 'noop'))
+            ->register(GraphqlNode::create('foo')->withResolver(fn () => 'bar'));
+
+        $this->registry->register(new Schema([
+            'query' => $this->registryBasedQueryType,
+        ]));
     }
 }
