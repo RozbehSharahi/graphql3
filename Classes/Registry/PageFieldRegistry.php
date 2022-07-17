44ec20fc2b847a4b3fa30af1ed4074fb435ded86
Rozbeh Chiryai Sharahi <rozbeh.sharahi@gmail.com>
2022-07-17 17:20:45 +0100

    [TASK] Setup core page fields in registry itself

--- Classes/Registry/PageFieldRegistry.php
+++ Classes/Registry/PageFieldRegistry.php
@@ -2,10 +2,12 @@
 
 namespace RozbehSharahi\Graphql3\Registry;
 
+use GraphQL\Type\Definition\Type;
 use RozbehSharahi\Graphql3\Domain\Model\GraphqlNode;
 use RozbehSharahi\Graphql3\Domain\Model\GraphqlNodeCollection;
+use RozbehSharahi\Graphql3\Setup\SetupInterface;
 
-class PageFieldRegistry
+class PageFieldRegistry implements SetupInterface
 {
     protected array $fields = [];
 
@@ -19,5 +21,19 @@ class PageFieldRegistry
     public function getFields(): GraphqlNodeCollection
     {
         return new GraphqlNodeCollection($this->fields);
+    }
+
+    public function setup(): void
+    {
+        $this
+            ->register(
+                GraphqlNode::create('uid')
+                    ->withType(Type::int())
+                    ->withResolver(fn (array $page) => $page['uid'])
+            )
+            ->register(
+                GraphqlNode::create('title')
+                    ->withResolver(fn (array $page) => $page['title'])
+            );
     }
 }
--- Classes/Type/RegistryBasedPageType.php
+++ Classes/Type/RegistryBasedPageType.php
@@ -3,8 +3,6 @@
 namespace RozbehSharahi\Graphql3\Type;
 
 use GraphQL\Type\Definition\ObjectType;
-use GraphQL\Type\Definition\Type;
-use RozbehSharahi\Graphql3\Domain\Model\GraphqlNode;
 use RozbehSharahi\Graphql3\Registry\PageFieldRegistry;
 use RozbehSharahi\Graphql3\Registry\TypeRegistry;
 use RozbehSharahi\Graphql3\Setup\SetupInterface;
@@ -22,16 +20,5 @@ class RegistryBasedPageType extends ObjectType implements SetupInterface
     public function setup(): void
     {
         $this->typeRegistry->register($this);
-
-        $this->pageFieldRegistry
-            ->register(
-                GraphqlNode::create('uid')
-                    ->withType(Type::int())
-                    ->withResolver(fn (array $page) => $page['uid'])
-            )
-            ->register(
-                GraphqlNode::create('title')
-                    ->withResolver(fn (array $page) => $page['title'])
-            );
     }
 }
