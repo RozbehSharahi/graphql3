8d6a480811597424afc50b9be22ce008baf17c99
Rozbeh Chiryai Sharahi <rozbeh.sharahi@gmail.com>
2022-08-09 22:40:50 +0100

    [FEATURE] Use query type on test-extension

--- Tests/Fixture/Graphql3TestExtension/Classes/GraphqlSetup.php
+++ Tests/Fixture/Graphql3TestExtension/Classes/GraphqlSetup.php
@@ -6,29 +6,23 @@ declare(strict_types=1);
 
 namespace RozbehSharahi\Graphql3TestExtension;
 
-use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Schema;
-use RozbehSharahi\Graphql3\Builder\Node\RecordNodeBuilder;
 use RozbehSharahi\Graphql3\Registry\SchemaRegistry;
 use RozbehSharahi\Graphql3\Setup\SetupInterface;
+use RozbehSharahi\Graphql3\Type\QueryType;
 
 class GraphqlSetup implements SetupInterface
 {
     public function __construct(
         protected SchemaRegistry $schemaRegistry,
-        protected RecordNodeBuilder $recordNodeBuilder
+        protected QueryType $queryType
     ) {
     }
 
     public function setup(): void
     {
         $this->schemaRegistry->register(new Schema([
-            'query' => new ObjectType([
-                'name' => 'Query',
-                'fields' => [
-                    'page' => $this->recordNodeBuilder->for('pages')->build()->toArray(),
-                ],
-            ]),
+            'query' => $this->queryType,
         ]));
     }
 }
